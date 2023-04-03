e689b0b0d5ebed548bcfe079166ad70aefa01276
Kay-El-41 <z.khantolino@gmail.com>
2023-04-03 21:07:10 +0630

    update

--- src/components/UI/MenuCard.jsx
+++ src/components/UI/MenuCard.jsx
@@ -12,7 +12,7 @@ const MenuCard = (props) => {
   return (
     <article className="relative w-[300px] h-[500px] lg:h-[400px] xl:h-[500px] rounded-t-xl bg-gray-lemon">
       <img
-        src={image}
+        src={require(image)}
         className="rounded-t-xl object-cover w-full h-[50%] lg:h-[35%] xl:h-[40%]"
       />
       <div className=" flex flex-col gap-5 p-5">
@@ -21,7 +21,10 @@ const MenuCard = (props) => {
           <p className="font-markazi text-2xl text-yellow-lemon">${price}</p>
         </div>
         <p className="font-karla text-sm">{description}</p>
-        <button className="font-karla absolute bottom-5 hover:text-yellow-lemon active:scale-95" onClick={addToOrder}>
+        <button
+          className="font-karla absolute bottom-5 hover:text-yellow-lemon active:scale-95"
+          onClick={addToOrder}
+        >
           Order a delivery
         </button>
       </div>
