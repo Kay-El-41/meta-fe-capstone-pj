6784e73ae8856c7cbd3d8c4f5450d045c6c88b74
Kay-El-41 <z.khantolino@gmail.com>
2023-04-04 15:30:58 +0630

    update

--- src/routes/ReservationPage.jsx
+++ src/routes/ReservationPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import OrderNavbar from '../components/orderPage/OrderNavbar'
 import ReservationForm from '../components/reservationPage/ReservationForm'
+import restaurantImg from '/src/assets/restaurant.jpg'
 
 const ReservationPage = () => {
   return (
@@ -10,7 +11,7 @@ const ReservationPage = () => {
       </header>
       <main className="px-[15px] lg:px-[150px] xl:px-[250px] flex flex-col lg:gap-10">
         <img
-          src="/src/assets/restaurant.jpg"
+          src={restaurantImg}
           className="rounded-xl object-cover h-[300px] mt-10"
         />
         <ReservationForm />
